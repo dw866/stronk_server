68644b0a3dd6287ecc80881cd17759838c06a63f
David Wu <dw866@drexel.edu>
2024-07-11 08:54:24 -0400

    Initial commit

---
+++ src/main/java/com/trenbologna/stronk/StronkApplication.java
@@ -0,0 +1,13 @@
+package com.trenbologna.stronk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StronkApplication.class, args);
+	}
+
+}
